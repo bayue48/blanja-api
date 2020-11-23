8e2d55689f04701e6a4f7b99fdf653f75dfd3e71
unknown <48yunyun@gmail.com>
2020-11-23 19:12:43 +0700

    prepare for modularization

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const mainRouter = express.Router();
 
 const welcomeRouter = require('./welcome');
 const productsRouter = require('./products');
+const productRouter = require('./product');
 const searchRouter = require('./search');
 const categoryRouter = require('./category');
 
 mainRouter.use('/', welcomeRouter); // localhost:8000
 mainRouter.use('/products', productsRouter); // localhost:8000/products
+mainRouter.use('/product', productRouter); // localhost:8000/product
 mainRouter.use('/search', searchRouter); // localhost:8000/search
 mainRouter.use('/category', categoryRouter); // localhost:8000/category
 
