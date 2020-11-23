62496013f31b12ea4a5c7cebe0f3034c9acda22f
unknown <48yunyun@gmail.com>
2020-11-23 13:47:58 +0700

    fixing routes and fix search by category

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const searchCategory = express.Router();
+const db = require('../config/mySQL');
 
-const categoryRouter = express.Router();
-const categoryController = require('../controllers/category');
-// req.query
+// req params
 
-// localhost:8000/category?{query}
-categoryRouter.get('/', categoryController.searchCategory);
+// localhost:8000/category/{params}
+searchCategory.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const getProductByCategory = new Promise((resolve, reject) => {
+        const qs = 
+            'SELECT p.id, p.product_name, p.product_brand, p.product_rating, p.product_desc, c.category_name, p.product_price, pc.color_name, s.sizes_name, p.product_qty, p.product_img, p.created_at, p.updated_at FROM products p JOIN categories c ON p.product_category = c.id JOIN colors pc ON p.product_color = pc.id JOIN sizes s ON p.product_size = s.id WHERE c.category_name = ?'    
+        db.query(qs, id, (err, data) => {
+            if (!err) {
+                resolve(data);
+            } else {
+                reject(err);
+            }
+        });
+    });
+    getProductByCategory
+        .then((data) => {
+            if (data.length) {
+                res.json(data[0]);
+            } else {
+                res.status(404).json({
+                msg: 'Data not Found',
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
 
-module.exports = categoryRouter;
+module.exports = searchCategory;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,11 @@ const mainRouter = express.Router();
 
 const welcomeRouter = require('./welcome');
 const productsRouter = require('./products');
-const productRouter = require('./product');
 const searchRouter = require('./search');
 const searchCategory = require('./category');
 
 mainRouter.use('/', welcomeRouter); // localhost:8000
 mainRouter.use('/products', productsRouter); // localhost:8000/products
-mainRouter.use('/product', productRouter); // localhost:8000/product
 mainRouter.use('/search', searchRouter); // localhost:8000/search
 mainRouter.use('/category', searchCategory); // localhost:8000/category
 
