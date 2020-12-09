8f2eb5b97e6b36d7473863602246dfa17df921b3
unknown <48yunyun@gmail.com>
2020-12-09 22:55:39 +0700

    fix transaction product id

--- src/models/history.js
+++ src/models/history.js
@@ -4,7 +4,7 @@ module.exports = {
     getHistory: () => {
         return new Promise((resolve, reject) => {
             const qs =
-                `SELECT th.id AS 'Transaction_ID', th.product AS 'Product_Name', c.category_name as 'Category', pc.color_name as 'Color', s.sizes_name AS 'Size', th.qty AS 'Quantity', th.price AS 'Price', (th.qty * th.price) AS 'Sub_Total', th.created_at AS 'Transaction_Date' FROM transactions_history th JOIN categories c ON th.category = c.id JOIN colors pc ON th.color = pc.id JOIN sizes s ON th.size = s.id ORDER BY th.created_at DESC`;
+                `SELECT th.id AS 'Transaction_ID', th.product AS 'Product_ID', c.category_name as 'Category', pc.color_name as 'Color', s.sizes_name AS 'Size', th.qty AS 'Quantity', th.price AS 'Price', (th.qty * th.price) AS 'Sub_Total', th.created_at AS 'Transaction_Date' FROM transactions_history th JOIN categories c ON th.category = c.id JOIN colors pc ON th.color = pc.id JOIN sizes s ON th.size = s.id ORDER BY th.created_at DESC`;
             db.query(qs, (err, data) => {
                 if (!err) {
                     resolve(data);
