1e10886364ee41afb0270ab60390bac81adee9a0
unknown <48yunyun@gmail.com>
2020-12-10 13:10:50 +0700

    fix history auth

--- src/models/history.js
+++ src/models/history.js
@@ -1,36 +1,42 @@
 const db = require('../config/mySQL');
 
 module.exports = {
-    getHistory: () => {
+    getHistory: (level) => {
         return new Promise((resolve, reject) => {
             const qs =
                 `SELECT th.id AS 'Transaction_ID', th.product AS 'Product_ID', c.category_name as 'Category', pc.color_name as 'Color', s.sizes_name AS 'Size', th.qty AS 'Quantity', th.price AS 'Price', (th.qty * th.price) AS 'Sub_Total', th.created_at AS 'Transaction_Date' FROM transactions_history th JOIN categories c ON th.category = c.id JOIN colors pc ON th.color = pc.id JOIN sizes s ON th.size = s.id ORDER BY th.created_at DESC`;
-            db.query(qs, (err, data) => {
+            if (level > 2) {
+                reject({
+                    msg: 'Unauthorized Access',
+                    status: 401
+                })
+            }
+            db.query(qs, level, (err, data) => {
                 if (!err) {
                     resolve(data);
                 } else {
                     reject(err);
                 }
-             });
+            });
         });
     },
     postHistory: (inserBody, level) => {
         return new Promise((resolve, reject) => {
             const qs =
                 'INSERT INTO transactions_history SET ?';
-                if (level > 2) {
-                    reject({
-                        msg: 'Unauthorized Access',
-                        status: 401
-                    })
-                }
+            if (level > 2) {
+                reject({
+                    msg: 'Unauthorized Access',
+                    status: 401
+                })
+            }
             db.query(qs, [inserBody, level], (err, data) => {
                 if (!err) {
                     resolve(data);
                 } else {
                     reject(err);
                 }
-             });
+            });
         });
     },
 };
--- src/routes/history.js
+++ src/routes/history.js
@@ -4,7 +4,7 @@ const historyRouter = express.Router();
 const historyController = require('../controllers/history')
 const verifyToken = require('../middlewares/verifyToken')
 
-historyRouter.get('/', historyController.getHistory)
+historyRouter.get('/', verifyToken, historyController.getHistory)
 historyRouter.post('/', verifyToken, historyController.postHistory)
 
 module.exports = historyRouter;
